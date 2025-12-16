2ffdd5feffce4d0d35f2b877e87fae8745db33f1
gisela benito <gisela.benito@estudiant.enti.cat>
2025-12-16 16:10:21 +0100

    Mix scenes

--- Assets/canvas_controller.cs
+++ Assets/canvas_controller.cs
@@ -5,12 +5,16 @@ public class canvas_controller : MonoBehaviour
 {
     [SerializeField] private GameObject model1;
     [SerializeField] private GameObject model2;
+    [SerializeField] private GameObject recorderScreen;
+    [SerializeField] private GameObject animationScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         model1.SetActive(false);
         model2.SetActive(false);
+       
+        animationScreen.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,5 +41,17 @@ public class canvas_controller : MonoBehaviour
         model2.SetActive(false);
         model1.SetActive(true);
         model2.SetActive(true);
+    }
+
+    public void ActiveRecorderScreen()
+    {
+        recorderScreen.SetActive(true);
+        animationScreen.SetActive(false);
+    }
+
+    public void ActiveAnimationScreen()
+    {
+        recorderScreen.SetActive(false);
+        animationScreen.SetActive(true);
     }
 }
